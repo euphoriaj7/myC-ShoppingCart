cf82627a8de254c4b901fb65e63ed0f61a938113
Jeff Pitcher <pit19004@byui.edu>
2022-05-24 13:00:56 -0600

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main()
+{
+	/// <summary>
+	/// cout stands for c++out statement 
+	/// </summary>
+	/// <returns></returns>
+	std::cout << "PRINT STATEMENTS" << std::endl;
+}
